4e49dfd22742ca1ac850a7b205de6e3a3a522e0d
Ira Bondar <irabondar1012@gmail.com>
2026-04-02 20:31:43 +0200

    resolve mobile responsive layout and card alignment issues

--- src/Components/Item.tsx
+++ src/Components/Item.tsx
@@ -17,14 +17,17 @@ type ItemProps = {
 export default function Item({ name, price, photo, rating, onSale, newPrice }: ItemProps) {
 
     const [active, setActive] = useState(false);
+
     return (
-        <div className="mx-auto max-w-[240px] sm:max-w-full h-97 pt-2.5 pb-4 px-2.5 hover:bg-gray-100 transition-colors duration-500 text-black rounded-2.5 flex flex-col justify-start items-start relative border border-gray-100"
+        <div className="w-full h-full pt-2.5 pb-4 px-2.5 hover:bg-gray-100 transition-colors duration-500 text-black rounded-2.5 flex flex-col justify-start items-start relative border border-gray-100"
             onMouseEnter={() => setActive(true)}
             onMouseLeave={() => setActive(false)}>
-            <img src={photo} alt="snicers" className="rounded-2.5 h-auto block" />
+            <div className="w-full overflow-hidden rounded-2.5 flex-shrink-1">
+                <img src={photo} alt="sneakers" className="w-full h-full object-cover block" />
+            </div>
             {onSale && <p className="px-3 py-1.5 bg-amber-400 rounded-2xl text-sm absolute top-5 right-5 font-bold">Disc {Math.round(100 - ((newPrice * 100) / price))} %</p>}
-            <div className="flex flex-col justify-between items-start mt-2.5 w-full h-full">
-                <h3 className="text-start font-bold" >{name}</h3>
+            <div className="flex flex-col justify-between items-start mt-2.5 w-full flex-grow gap-8">
+                <h3 className="text-start font-bold w-full break-words line-clamp-2" >{name}</h3>
                 {onSale === true && price !== newPrice ?
                     <div className=" text-left sm:flex gap-4 sm:items-center">
                         <p className="line-through">{price} Kč</p>
@@ -36,8 +39,7 @@ export default function Item({ name, price, photo, rating, onSale, newPrice }: I
                         <img src={starRate} alt="arrow" className="w-7" />
                         <p>{rating}/5</p>
                     </div>
-                    <div className={`transition-all duration-300 ease-in-out ${active ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2 pointer-events-none"
-                        } lg:opacity-100 lg:translate-y-0 lg:pointer-events-auto`}>
+                    <div className={`transition-all duration-300 ease-in-out opacity-100 translate-y-0 pointer-events-auto ${active ? "lg:opacity-100 lg:translate-y-0" : "lg:opacity-0 lg:translate-y-2 lg:pointer-events-none"}`}>
                         <Button
                             text="Shop Now"
                             active={true}
